f8e40af38bca11973948bffb0766cf9f3764b2b3
Shivam Maddheshiya <shivam70686670@gmail.com>
2025-11-21 17:34:19 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
 
         <div className=" bg-purple-100 flex flex-col gap-4 items-center justify-center text-center">
           <p className="text-4xl font-bold font-serif">The best url shortner in market</p>
-          <p className=" px-11 text-2xl text-center font-medium">We are best url shortner in the word. In most of url's shortner you have to login but we understand your needs you can shorten your urls easily</p>
+          <p className=" px-11 text-2xl text-center font-medium">We are best url shortner in the word. In most of urls shortner you have to login but we understand your needs you can shorten your urls easily</p>
 
           <li className='flex gap-3'>
             <Link href="/Shorten"><button className='bg-purple-500 rounded-lg p-3 py-1 font-bold text-white'>Try now</button></Link>
