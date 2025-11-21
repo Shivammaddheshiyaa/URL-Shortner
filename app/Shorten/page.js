d813c2bdc3efc50c6e0523b98ce42e26785328be
Shivam Maddheshiya <shivam70686670@gmail.com>
2025-11-21 17:37:01 +0530

    Fix placeholder text for input fields

--- app/Shorten/page.js
+++ app/Shorten/page.js
@@ -44,8 +44,8 @@ fetch("/api/generate", requestOptions)
         <div className='mx-auto max-w-2xl bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
             <h1 className='font-bold text-2xl text-center'>Generate your short URLs</h1>
             <div className='flex flex-col gap-2'>
-                <input type="text" value={url} className='bg-white px-4 py-2 focus:outline-purple-600' placeholder='Enter your URL' onChange={e => { seturl(e.target.value) }} />
-                <input type="text" value={shorturl} className='px-4 py-2 focus:outline-purple-600 bg-white' placeholder='Enter your prefered short URL' onChange={e => { setshorturl(e.target.value) }} />
+                <input type="text" value={url} className='bg-white px-4 py-2 focus:outline-purple-600' placeholder="Enter your URL" onChange={e => { seturl(e.target.value) }} />
+                <input type="text" value={shorturl} className='px-4 py-2 focus:outline-purple-600 bg-white' placeholder="Enter your prefered short URL" onChange={e => { setshorturl(e.target.value) }} />
                 <button onClick={generate} className='bg-purple-600 rounded-lg p-3 py-1 font-bold text-white my-3'>Generate</button>
             </div>
 
